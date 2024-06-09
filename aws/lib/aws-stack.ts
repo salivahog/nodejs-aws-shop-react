f98137e7e6ba70071ed9dac45a21064edd3d0384
salivahog <tiarted@gmail.com>
2024-06-09 17:46:34 +0200

    feat:deployment buckets3

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam'
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const spaBucket = new s3.Bucket(this, 'rsschoolBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+
+    new s3deploy.BucketDeployment(this, 'DeploymentBucket', {
+      sources: [s3deploy.Source.asset('../dist')],
+      destinationBucket: spaBucket,
+
+      distributionPaths: ['/*'],
+    });
+
   }
 }
